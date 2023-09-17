761bae3c7bb97bc785f2df4394781c05f59ff595
benzWRC <worachai3@live.com>
2023-09-18 03:27:10 +0700

    Edit terraform

--- main.tf
+++ main.tf
@@ -8,6 +8,19 @@ terraform {
   }
 }
 
+variable "AWS_ACCESS_KEY_ID" {
+    type = string
+}
+variable "AWS_SECRET_ACCESS_KEY" {
+    type = string
+}
+variable "AWS_DEFAULT_REGION" {
+    type = string
+}
+variable "ECR_REPOSITORY" {
+    type = string
+}
+
 provider "aws" {
   region = "us-east-1"
 }
